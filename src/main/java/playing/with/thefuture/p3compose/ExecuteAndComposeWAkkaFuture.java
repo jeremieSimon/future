022a87b7c083d9ca30ae4e2bfb635e56af42e978
jeremie <je.simon@criteo.com>
2015-07-24 15:47:49 -0700

    example w Akka

---
+++ src/main/java/playing/with/thefuture/p3compose/ExecuteAndComposeWAkkaFuture.java
@@ -0,0 +1,67 @@
+package playing.with.thefuture.p3compose;
+
+import static akka.dispatch.Futures.future;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import scala.concurrent.Await;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+import akka.dispatch.ExecutionContexts;
+import akka.dispatch.Mapper;
+import akka.util.Timeout;
+
+/**
+ * Now, let's see how we can add some extra logic without blocking. Akka future are more composable,
+ * and we can pipe operations to it. Here we just try to negate the values and filter the odd ones
+ */
+public class ExecuteAndComposeWAkkaFuture {
+
+    public static void main(final String[] args) throws Exception {
+        final int numberOfTasks = 10;
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        ExecutionContext execContext = ExecutionContexts
+            .fromExecutorService(service);
+
+        // populate with dummy tasks
+        // here, a task has a change to fail
+        Queue<Callable<Integer>> tasks = new LinkedList<>();
+        for (int i = 0; i < numberOfTasks; i++) {
+            tasks.add(() -> {
+                return new Random().nextInt(2000);
+            });
+        }
+
+        List<Future<Integer>> futures = executeTask(execContext, tasks);
+        List<Future<Integer>> asNegVals = futures.stream()
+            .map(f -> f.map(new Mapper<Integer, Integer>() {
+                @Override
+                public Integer apply(Integer i) {
+                    return -i;
+                }
+            }, execContext)).collect(Collectors.toList());
+
+        Timeout timeout = new Timeout(Duration.create(5, "seconds"));
+        for (Future<Integer> f: asNegVals) {
+            System.out.println(Await.result(f, timeout.duration()));
+        }
+        service.shutdown();
+    }
+
+    public static List<Future<Integer>>
+        executeTask(final ExecutionContext execContext,
+            final Queue<Callable<Integer>> tasks) {
+
+        return tasks.stream().map((t) -> future(t, execContext))
+            .collect(Collectors.toList());
+    }
+
+}
